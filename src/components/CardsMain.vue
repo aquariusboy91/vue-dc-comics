ab360ef36c72014341efc8baf9f1c3fa8e8eccf1
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-01-14 16:23:48 +0100

    rev margin cards

--- src/components/CardsMain.vue
+++ src/components/CardsMain.vue
@@ -19,11 +19,12 @@ export default {
 
 <style lang="scss">
     .container-cards {
-      flex-basis: calc((100% / 6) - 1em);
+      flex-basis: calc((100% / 6) - 2em);
       display: flex;
       flex-direction: column;
       background-color: black;
       align-items: center;
+      margin: 0em 0.5em 0em 0.5em;
       img {
         width: 80%;
         height: 80%;
